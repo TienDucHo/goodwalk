5cb1d29559f20de64d3775060b2f757cb1d29fda
mansooranis <mansooranis60@gmail.com>
2022-10-02 09:11:57 -0600

    made modification to the route

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -36,7 +36,7 @@ router.get("/steps/:username", async(req,res)=>{
 
         const user = await User.findOne({username:req.params.username})
         //console.log(user)
-        res.send({steps: user.stepcounter})
+        res.send({steps: user.stepcounter, user:user})
     }catch(err){
         console.log(err)
     }
