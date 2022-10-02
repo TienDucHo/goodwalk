649639f372c721719d257c38225fb85881e43006
Duc Ho <hotienduc05012002@gmail.com>
2022-10-02 05:56:07 -0600

    Fixed minor bugs

--- backend/routes/User.js
+++ backend/routes/User.js
@@ -2,59 +2,64 @@ const router = require("express").Router();
 const User = require("../models/UserModel");
 const bcrypt = require("bcryptjs");
 
-router.post("/register", async (req, res)=>{
-    const salt = await bcrypt.genSalt(10);
-    console.log(req.body)
-    try{
-        const hashPassword = await bcrypt.hash(req.body.password, salt);
-        const user = new User({
-            username: req.body.username,
-            name: req.body.name,
-            password: hashPassword,
-        })
-        const savedUser = await user.save();
-        console.log(req.body.username, "registered", savedUser)
-        res.send("User created")
-    }catch(err){
-        console.log(err)
-        res.send(err).status(400)
-    }
-})
-
-router.post("/login", async (req, res) => {
-    const user = await User.findOne({ username: req.body.username });
-    if (!user) return res.status(400).send("Email or Password is Incorrect!")
-
-    const validPass = await bcrypt.compare(req.body.password, user.password);
-    if (!validPass) return res.status(400).send("Email or Password is Incorrect!");
-
-    res.sendStatus(200).send(req.body.username)
+router.post("/register", async (req, res) => {
+  const salt = await bcrypt.genSalt(10);
+  console.log(req.body);
+  try {
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
+    const user = new User({
+      username: req.body.username,
+      name: req.body.name,
+      password: hashPassword,
+    });
+    const savedUser = await user.save();
+    console.log(req.body.username, "registered", savedUser);
+    res.send("User created");
+  } catch (err) {
+    console.log(err);
+    res.send(err).status(400);
+  }
 });
 
-router.get("/steps/:username", async(req,res)=>{
-    try{
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user)
+    return res.status(400).send("Username or Password is Incorrect!");
 
-        const user = await User.findOne({username:req.params.username})
-        //console.log(user)
-        res.send({steps: user.stepcounter})
-    }catch(err){
-        console.log(err)
-    }
-})
+  const validPass = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+  if (!validPass)
+    return res.status(400).send("Username or Password is Incorrect!");
 
-router.post("/steps/:username", async(req,res)=>{
-    try{
-        const user = await User.findOne({username:req.params.username})
-        
-        const totalsteps = user.stepcounter + req.body.steps
-        var myquery = { username: user.username };
-        var newvalues = { $set: { stepcounter: totalsteps } };
-        User.updateOne(myquery,newvalues,function(err, ress) {
-            res.send("done")
-        })
-    
-    }catch(err){
+  res.status(200).send(user.name);
+});
 
-    }
-})
+router.get("/steps/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.params.username,
+    });
+    //console.log(user)
+    res.send({ steps: user.stepcounter });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/steps/:username", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.params.username,
+    });
+
+    const totalsteps = user.stepcounter + req.body.steps;
+    var myquery = { username: user.username };
+    var newvalues = { $set: { stepcounter: totalsteps } };
+    User.updateOne(myquery, newvalues, function (err, ress) {
+      res.send("done");
+    });
+  } catch (err) {}
+});
 module.exports = router;
