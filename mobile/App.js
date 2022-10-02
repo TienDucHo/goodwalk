db12267b1e7a10f314cfbbcef31f85fbf9e565a4
Duc Ho <hotienduc05012002@gmail.com>
2022-10-01 18:24:47 -0600

    Resolve conflict

--- mobile/App.js
+++ mobile/App.js
@@ -1,9 +1,24 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState } from "react";
+<<<<<<< HEAD
 import { Text, View } from "react-native";
 
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-slate-200"></View>
+=======
+import { SafeAreaView, Text, View } from "react-native";
+import { Pedometer } from "expo-sensors";
+import Login from "./pages/login";
+
+
+export default function App() {
+  return (
+    <SafeAreaView className="">
+      
+      <StatusBar style="auto" />
+      <Login/>
+    </SafeAreaView>
+>>>>>>> 6e696d6 (added new npm library twrnc)
   );
 }
--- mobile/tailwind.config.js
+++ mobile/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+<<<<<<< HEAD
   content: ["./App.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+=======
+  content: [
+    "./App.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+  ],
+>>>>>>> 6e696d6 (added new npm library twrnc)
   theme: {
     extend: {},
   },
